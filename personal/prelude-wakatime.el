b0df44a2c63dcdc1a5ec63b23f94ff55c17f0a17
shixiongfei <jenson.shixf@gmail.com>
2019-11-08 14:51:11 +0800

    Enable WakaTime module

---
+++ personal/prelude-wakatime.el
@@ -0,0 +1,10 @@
+;;; prelude-wakatime.el --- Emacs Prelude: wakatime-mode configuration.
+;;
+;; Author: shixiongfei
+
+(prelude-require-packages '(wakatime-mode))
+(global-wakatime-mode)
+
+(provide 'prelude-wakatime)
+
+;;; prelude-wakatime.el ends here
