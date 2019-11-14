ab8b753f425471d76be79af78920c82a79ce3652
shixiongfei <jenson.shixf@gmail.com>
2019-11-14 17:33:06 +0800

    Set cursor type

--- personal/preload/prelude-environment.el
+++ personal/preload/prelude-environment.el
@@ -5,6 +5,9 @@
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
 
+;; Set cursor type
+(setq-default cursor-type 'bar)
+
 ;; Setup shell exec path
 (if (eq system-type 'windows-nt)
     ;; For autopep8 on Windows
