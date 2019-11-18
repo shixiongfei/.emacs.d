9bdc904b8c260bd27a619c4c41f3a6c33ca7047d
shixiongfei <jenson.shixf@gmail.com>
2019-11-18 14:49:39 +0800

    Set Emacs font & maximized on start up

--- personal/preload/prelude-environment.el
+++ personal/preload/prelude-environment.el
@@ -9,6 +9,12 @@
 (global-display-line-numbers-mode)
 (global-visual-line-mode)
 
+;; Set frame font
+(set-frame-font "Sarasa Mono SC 12")
+
+;; The frame is maximized when Emacs starts up
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
 ;; Setup shell exec path
 (if (eq system-type 'windows-nt)
     ;; For autopep8 on Windows
