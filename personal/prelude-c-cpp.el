22178f832beabc03ecc165131817749d71f81c8c
shixiongfei <jenson.shixf@gmail.com>
2019-11-20 16:30:00 +0800

    clang-format

---
+++ personal/prelude-c-cpp.el
@@ -0,0 +1,15 @@
+;;; prelude-c-cpp.el --- Emacs Prelude: C/C++ Configuration
+;;
+;; Author: shixiongfei
+
+(prelude-require-packages '(clang-format))
+
+(defun clang-format-on-save ()
+  (add-hook 'before-save-hook #'clang-format-buffer nil 'local))
+
+(add-hook 'c-mode-hook 'clang-format-on-save)
+(add-hook 'c++-mode-hook 'clang-format-on-save)
+
+(provide 'prelude-c-cpp)
+
+;;; prelude-c-cpp ends here
