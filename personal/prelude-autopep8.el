f80538c3e5b94d368b9bb35d43eb801635dbfdcc
shixiongfei <jenson.shixf@gmail.com>
2019-12-25 17:50:44 +0800

    set python shell interpreter to "python3" on macos

--- personal/prelude-autopep8.el
+++ personal/prelude-autopep8.el
@@ -5,6 +5,9 @@
 (prelude-require-packages '(py-autopep8))
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
+(if (eq system-type 'darwin)
+  (setq python-shell-interpreter "python3"))
+
 (provide 'prelude-autopep8)
 
 ;;; prelude-autopep8.el ends here
