cb6c236486b5f671e4f2d0ffd6ddeced34f7640f
shixiongfei <jenson.shixf@gmail.com>
2020-07-05 12:28:46 +0800

    lua-mode setup

--- personal/prelude-lua.el
+++ personal/prelude-lua.el
@@ -6,9 +6,12 @@
 
 (with-eval-after-load 'lua-mode
   (setq lua-indent-level 2)
+  (setq lua-indent-nested-block-content-align nil)
+  (setq lua-indent-close-paren-align nil)
   (define-key lua-mode-map "\C-c\C-c" 'lua-send-current-line)
   (define-key lua-mode-map "\C-c\C-e" 'lua-send-region)
   (define-key lua-mode-map "\C-c\C-z" 'lua-show-process-buffer))
+
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
