cfbccfba46ce52d47191e4b9b677bfe4a6c86289
shixiongfei <jenson.shixf@gmail.com>
2020-08-24 22:47:51 +0800

    add keyfreq

---
+++ personal/prelude-keyfreq.el
@@ -0,0 +1,12 @@
+;;; prelude-keyfreq.el --- Emacs Prelude: KeyFreq Configuration
+;;
+;; Author: shixiongfei
+
+(prelude-require-packages '(keyfreq))
+
+(keyfreq-mode 1)
+(keyfreq-autosave-mode 1)
+
+(provide 'prelude-keyfreq)
+
+;;; prelude-keyfreq ends here
