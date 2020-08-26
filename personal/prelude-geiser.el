5483f4dfff5d642ecf74eea49128ed7fc821cf56
shixiongfei <jenson.shixf@gmail.com>
2020-08-26 19:21:47 +0800

    add gambit-scheme

---
+++ personal/prelude-geiser.el
@@ -0,0 +1,9 @@
+;;; prelude-geiser.el --- Emacs Prelude: Geiser Configuration
+;;
+;; Author: shixiongfei
+
+(setq geiser-active-implementations '(gambit))
+
+(provide 'prelude-geiser)
+
+;;; prelude-geiser ends here
