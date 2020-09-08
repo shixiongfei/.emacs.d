c1319ca2887309f6d6a61e91ecc7e8de7f95619c
shixiongfei <jenson.shixf@gmail.com>
2020-09-08 21:02:47 +0800

    update module

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -25,7 +25,7 @@
 (require 'prelude-latex)
 (require 'prelude-lisp)
 (require 'prelude-lsp)
-(require 'prelude-ocaml)
+;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 (require 'prelude-python)
