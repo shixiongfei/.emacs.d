4b3fa9e218f0656dda773a405577afd69514fc3c
shixiongfei <xiongfei.shi@icloud.com>
2021-03-14 00:14:26 +0800

    Better JavaScript support

---
+++ personal/prelude-javascript.el
@@ -0,0 +1,27 @@
+;;; prelude-javascript.el --- Emacs Prelude: JavaScript programming support.
+;;
+;; Author: shixiongfei
+
+(prelude-require-packages '(js2-refactor
+                            xref-js2
+                            js2-highlight-vars))
+
+;; js2-refactor
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
+;; xref-js2
+(define-key js2-mode-map (kbd "M-.") nil)
+(add-hook 'js2-mode-hook
+  (lambda () (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+(with-eval-after-load 'js2-refactor-mode
+  (setq js2-skip-preprocessor-directives t))
+
+;; js2-highlight-vars
+(eval-after-load "js2-highlight-vars-autoloads"
+  '(add-hook 'js2-mode-hook (lambda () (js2-highlight-vars-mode))))
+
+(provide 'prelude-javascript)
+
+;;; prelude-javascript ends here
--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -37,7 +37,7 @@
 ;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
-;; (require 'prelude-ts)
+(require 'prelude-ts)
 (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 (require 'prelude-yaml)
