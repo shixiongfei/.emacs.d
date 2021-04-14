296adbe4f7bc1da1cca04ae6b999c8603185270f
shixiongfei <xiongfei.shi@icloud.com>
2021-04-15 00:45:56 +0800

    setup for chezscheme

---
+++ personal/prelude-chezscheme.el
@@ -0,0 +1,17 @@
+;;; prelude-chezscheme.el --- Emacs Prelude: Chez Scheme Configuration
+;;
+;; Author: shixiongfei
+
+
+(prelude-require-packages '(geiser-chez))
+
+(require 'geiser-chez)
+
+(if (eq system-type 'darwin)
+    (setq geiser-chez-binary "chez"))
+
+(setq geiser-active-implementations '(chez))
+
+(provide 'prelude-geiser)
+
+;;; prelude-chezscheme ends here
